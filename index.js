4fab17b98d8d2c96241adb67e3191affe2bf4d13
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-05-16 17:01:54 -0700

    commit tree 1645eb2d21b667f363ca89bd90114c0e8340f7fb

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number) {
+  // Write your code here!
+  if (number <= 400) {
+    return "This one is on me!";
+  } else if (number <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (2000 < number && number < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
